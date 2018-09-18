ae99c332527dec30d60c7ebb97c41a51a6215308
JimF99 <jimfrem99@gmail.com>
2018-09-18 10:24:27 -0600

    Included static json files to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -379,6 +379,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '*.html',
+            '*.json',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
           ]
