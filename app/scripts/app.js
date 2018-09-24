23387e0c8bfae86e3b17730adaa879c22ecf3ad6
JimF99 <jimfrem99@gmail.com>
2018-09-24 14:14:51 -0600

    Added Angular-xeditable

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,42 +9,57 @@
  * Main module of the application.
  */
 angular
-    .module('angTestApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/customers', {
-        templateUrl: 'views/customers.html',
-        controller: 'CustomersCtrl',
-        controllerAs: 'customers'
-      })
-      .when('/calculator', {
-        templateUrl: 'views/calculator.html',
-        controller: 'CalculatorCtrl',
-        controllerAs: 'calculator'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        //controller: 'LoginCtrl',
-        //controllerAs: 'login'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .module(
+	'angTestApp',
+	[
+		'ngAnimate',
+		'ngCookies',
+		'ngResource',
+		'ngRoute',
+		'ngSanitize',
+		'ngTouch',
+		'xeditable'
+	]
+  ) /* end module declaration */
+  .config(
+	  function ($routeProvider) {
+		$routeProvider
+		  .when('/', {
+			templateUrl: 'views/main.html',
+			controller: 'MainCtrl',
+			controllerAs: 'main'
+		  })
+		  .when('/about', {
+			templateUrl: 'views/about.html',
+			controller: 'AboutCtrl',
+			controllerAs: 'about'
+		  })
+		  .when('/customers', {
+			templateUrl: 'views/customers.html',
+			controller: 'CustomersCtrl',
+			controllerAs: 'customers'
+		  })
+		  .when('/calculator', {
+			templateUrl: 'views/calculator.html',
+			controller: 'CalculatorCtrl',
+			controllerAs: 'calculator'
+		  })
+		  .when('/login', {
+			templateUrl: 'views/login.html',
+			//controller: 'LoginCtrl',
+			//controllerAs: 'login'
+		  })
+		  .otherwise({
+			redirectTo: '/'
+		  });
+	  }
+  ) /* end config */
+  .run(
+      [
+	      'editableOptions',
+		  function(editableOptions) {
+             editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+	      }
+	  ] // end array
+  ); /* end run  */
+
